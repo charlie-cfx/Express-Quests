a177163648ff6b205df698b4eda29c65c7d8925c
Charlie FEIX <contact@charliefeix.fr>
2023-06-19 10:29:57 +0200

    Add JWT authentification

--- app.js
+++ app.js
@@ -8,25 +8,31 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+
 const welcome = (req, res) => {
-    res.send("Welcome to my favourite movie list");
+    res.send("Welcome to my favorite movie list");
 };
 
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+const userHandlers = require("./userHandlers");
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+
+app.use(verifyToken);
+
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
-const { hashPassword } = require("./auth.js");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
